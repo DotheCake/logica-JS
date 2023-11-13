a00d433c7efad3c0e9c3b6a1629a227885cca854
DotheCake <142364407+DotheCake@users.noreply.github.com>
2023-11-13 23:21:46 +0000

    laços de repetição

---
+++ teste.js
@@ -0,0 +1,37 @@
+console.log("Teste")
+// Estrutura For (usado para executar o codigo varias vezes, otimo paa casos que você sabe a quantidade que precisa ser rodado)
+
+for (let i = 0; i < 5; i++){
+    console.log("Número " + i);
+}
+// variavel i começa em 0 e vai aumentando em 1 até dar 5
+
+//Estrutura While (Usado quando você não sabe quantas vezes ele precisa rodar)
+
+//let contador = 0;
+//while (contador < 5 ){
+  //  console.log("Contador " + contador); contador++;
+//}
+
+// Estrutura Do-While (aplica o codigo 1 vez pelo menos e ai verifica a condição!)
+let x = 0;
+do {
+    console.log("Número: " + x);x++;
+} while (x < 5);
+
+
+/////
+let contador = 1
+while(contador <= 5){
+    console.log(contador)
+    contador ++
+}
+
+while (contador >=1){
+    console.log(contador)
+    contador --
+}
+
+
+///
+
