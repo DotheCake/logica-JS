749e39be98b9607eeaf3fc81025d7810d8c48e4c
DotheCake <142364407+DotheCake@users.noreply.github.com>
2023-11-14 00:56:59 +0000

    atividade 03/04

---
+++ atividade03.js
@@ -0,0 +1,12 @@
+// laço até
+
+let i = 0
+for (i = i ; i<=10 ; i++){
+    console.log(i)
+}
+
+console.log ("=====>" + i)
+
+for (let i = 0 ; i <= 5; i++){
+    console.log("Botafogo Campeão" + i)
+}
---
+++ atividade04.js
@@ -0,0 +1,11 @@
+let frutas = ["🍇","🍓","🍌","🍐","🍏"]
+
+for ( let i = 0; i < frutas.length; i++){
+    console.log (frutas[i])
+}
+
+// let i = 0
+// while (i < frutas.length){
+//    console.log(frutas[i]
+//        i++)
+//}
