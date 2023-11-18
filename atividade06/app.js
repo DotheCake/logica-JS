708f6a72d49c09824505f3dd8ab0a5d4668a4977
DotheCake <marianafmollo@hotmail.com>
2023-11-17 22:19:09 -0300

    DOM

---
+++ atividade06/app.js
@@ -0,0 +1,17 @@
+function escreveTexto(){
+    document.getElementById("titulo").innerText="Curso de Javascript"
+    document.getElementById("texto").innerText="Estou estudando Javascript e esepero aprender, ou pelo menos o guto espera que eu aprenda."
+}
+
+function escreveOi(mensagem){
+    document.write(mensagem)
+}
+
+function corFundo(cor){
+    document.body.style.backgroundColor=cor
+}
+
+
+// escreveTexto()
+// corFundo("violet")
+
