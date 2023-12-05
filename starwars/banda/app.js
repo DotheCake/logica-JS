7ff07162a394ebfca6a85ce0a40ea2f6e442cca6
DotheCake <marianafmollo@hotmail.com>
2023-12-04 22:18:27 -0300

    exemplo usando interesse em bandas

---
+++ starwars/banda/app.js
@@ -0,0 +1,14 @@
+function butter (){
+    document.getElementById ("mensagem").innerText = "Smooth like butter"
+    document.getElementById ("frase").innerText = "Lançado em 2021"
+    document.getElementById ("imagem").src = "butter.png"
+    document.body.style = "background: #FFD700"
+}
+
+function proof(){
+    document.getElementById("mensagem").innerText = "Ultimo album antes do hiatos para o alistamento obrigatorio"
+    document.getElementById ("frase").innerText = "Lançado em 2022"
+    document.getElementById ("imagem").src = "proof.jpg"
+    document.body.style = "background: #000000; color: #F8F8FF "
+
+}
